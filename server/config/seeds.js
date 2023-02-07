132d866d953f45274b87b4a838c2a256b02fe6f8
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-02-07 03:17:39 -0600

    added descriptions

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -27,25 +27,25 @@ db.once('open', async () => {
       quantity: 500
     },
     {
-      name: 'Tools of Titans',
+      name: `Tools of Titans`,
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+        `Tools of Titans is a book written by Tim Ferriss, a bestselling author, entrepreneur, and podcast host. The book is a compilation of tactics, routines, and habits of high-performing individuals, including business leaders, athletes, and celebrities, which Ferriss has gathered from his podcast, "The Tim Ferriss Show". The book covers topics such as productivity, health, and wealth, and provides insights and actionable advice on how to optimize one's life. It's a comprehensive guide aimed at helping readers improve their personal and professional lives by adopting the habits and strategies of successful individuals.`,
       image: 'canned-coffee.jpg',
       genre: genres[0],
       price: 1.99,
       quantity: 500
     },
     {
-      name: 'The Alchemist',
+      name: `The Alchemist`,
       genre: genres[1],
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
+      `The Alchemist is a novel written by Paulo Coelho, a Brazilian author. It was first published in 1988 and has since become a worldwide bestseller. The book follows the journey of Santiago, a shepherd boy who dreams of discovering a treasure as old as the universe. The story is a blend of adventure, self-discovery, and spiritual enlightenment, and it explores themes of personal growth, the pursuit of one's destiny, and the power of the universe to guide and fulfill our lives. Through Santiago's journey, the reader is inspired to follow their own personal legend and find joy, fulfillment, and wisdom along the way. The book has been widely acclaimed for its simple yet profound message, and its ability to inspire readers to seek their own path in life.`,
       image: 'toilet-paper.jpg',
       price: 7.99,
       quantity: 20
     },
     {
-      name: 'The Da Vinci Code',
+      name: `The Da Vinci Code`,
       genre: genres[1],
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
@@ -54,28 +54,28 @@ db.once('open', async () => {
       quantity: 50
     },
     {
-      name: 'The Richest Man in Babylon',
+      name: `The Richest Man in Babylon`,
       genre: genres[1],
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+      `The Da Vinci Code is a mystery-detective novel written by Dan Brown. It was first published in 2003 and became a global bestseller. The book follows the story of symbologist Robert Langdon as he investigates a murder at the Louvre Museum in Paris and uncovers a conspiracy to protect the secret of the Holy Grail. The novel combines elements of conspiracy theories, religious symbolism, and codes to create a fast-paced and suspenseful storyline that takes the reader on a journey through the hidden world of secret societies and ancient conspiracies. Along the way, Langdon and his companion, Sophie Neveu, uncover a secret that could shake the foundations of Western civilization and challenge the traditional view of history. The book has been both praised for its imaginative plot and criticized for its historical inaccuracies and controversial subject matter. Regardless, "The Da Vinci Code" remains a popular and widely-read book, and has been adapted into a feature film.`,
       image: 'wooden-spoons.jpg',
       price: 14.99,
       quantity: 100
     },
     {
-      name: 'Atomic Habits',
+      name: `Atomic Habits`,
       genre: genres[2],
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
+      `Atomic Habits is a self-help book written by James Clear. It was published in 2018 and has since become a popular resource for individuals seeking to improve their habits and achieve their goals. The book focuses on the idea that small, incremental changes to one's behavior can lead to significant improvements in life. Clear provides practical advice and strategies for developing and maintaining habits, and he emphasizes the importance of focusing on process rather than outcome. He also explains how habits work and how to break bad habits and replace them with good ones. The book covers a range of topics, including health, productivity, relationships, and personal finance, and provides a framework for designing and maintaining habits that support one's goals. With its emphasis on the power of small changes and its practical approach, "Atomic Habits" has been praised by readers as a valuable and accessible guide to habit change.`,
       image: 'camera.jpg',
       price: 399.99,
       quantity: 30
     },
     {
-      name: 'The 48 Laws of Power',
+      name: `The 48 Laws of Power`,
       genre: genres[2],
       description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
+      `The 48 Laws of Power" is a book written by Robert Greene. It was first published in 1998 and has since become a widely-read book on power and strategy. The book is a collection of 48 laws, or principles, that have been used throughout history by individuals seeking to gain and maintain power. The laws range from the devious, such as "Conceal Your Intentions," to the more ethical, such as "Think As You Like But Behave Like Others." The book provides historical examples to illustrate each law and offers guidance on how to apply these principles in the modern world. The book has been described as a guide for navigating the complex and often ruthless world of power and politics, and it has been both praised for its insight and criticized for its ruthless and Machiavellian philosophy. Regardless, "The 48 Laws of Power" remains a popular and widely-read book, and has been referred to as a classic in the field of power and strategy.`,
       image: 'tablet.jpg',
       price: 199.99,
       quantity: 30
